3d16bba3cbb2b167a63f79aaac52a193eb6bcadf
jandie1505 <34031551+jandie1505@users.noreply.github.com>
2025-05-17 14:51:45 +0200

    added javadocs

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"Alle Packages","u":"allpackages-index.html"},{"l":"net.chaossquad.mclib.packets"},{"l":"net.chaossquad.mclib.packets.packetentity"},{"l":"net.chaossquad.mclib.packets.packetevents"}];updateSearchResults();
